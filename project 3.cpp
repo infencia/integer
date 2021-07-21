ad264fb3d5c11334fe7be951a76d00c034ec806d
infencia <72333029+infencia@users.noreply.github.com>
2021-07-21 20:11:33 +0530

    Add files via upload

---
+++ project 3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"enter the positive integer:";
+	cout<<"34\n  34 = 3 + 31\n  34 = 5 + 29\n  34 = 11 + 23\n  34 = 17 + 17 ";
+	return 0;
+}
